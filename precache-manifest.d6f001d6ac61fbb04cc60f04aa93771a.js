28580b6fa646872a4f2c1bcc965d502453292818
ikaub <dimanovikov22@gmail.com>
2020-10-10 07:25:58 +0300

    Updates

---
+++ precache-manifest.d6f001d6ac61fbb04cc60f04aa93771a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f529dd3ca25904062d0ed3283201c531",
+    "url": "/blog-space/index.html"
+  },
+  {
+    "revision": "2831b23c4e4d35325610",
+    "url": "/blog-space/static/css/main.a87b37ad.chunk.css"
+  },
+  {
+    "revision": "456d3144490bdb8b35cc",
+    "url": "/blog-space/static/js/2.672bc9bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/blog-space/static/js/2.672bc9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2831b23c4e4d35325610",
+    "url": "/blog-space/static/js/main.9b671ff1.chunk.js"
+  },
+  {
+    "revision": "c00ff6fbcef41d700475",
+    "url": "/blog-space/static/js/runtime-main.a132415c.js"
+  }
+]);
